ed2672c1c3d10afb75f1310efc361c2c5fe71fff
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-06-29 13:58:05 -0700

    commit tree 16f90fa0c6e910d391405a063fe0f287efc361d7

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo")
+}
+
+function appendCat(){
+    let newCat = ["Broom"]
+    let newCats = cats.concat(newCat)
+    return newCats
+}
+
+function prependCat(cat){
+    let newCats = [...cats]
+    newCats.unshift(cat)
+    return newCats
+}
+
+function removeLastCat(){
+    let newCats = [...cats]
+    newCats.pop()
+    return newCats
+}
+
+function removeFirstCat(){
+    let newCats = [...cats]
+    newCats.shift()
+    return newCats
+}
